50f8d5ac1f55acd13cbda3659b93e80ab10a0f07
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2024-10-01 16:13:30 +0200

    Get rid of dummy text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,18 +2,15 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Remix Application Template" },
-    { name: "description", content: "Hello DigitalService!" },
+    { title: "Verfahrensplattform" },
+    { name: "description", content: "Hello Verfahrensplattform!" },
   ];
 };
 
 export default function Index() {
   return (
     <main className={"flex flex-col items-center m-40"}>
-      <h1 className={"ds-heading-01-reg mb-40"}>Hello DigitalService!</h1>
-      <button className="ds-button ds-button-large">
-        <span className="ds-button-label">Click me for nothing</span>
-      </button>
+      <h1 className={"ds-heading-01-reg mb-40"}>Hello Verfahrensplattform!</h1>
     </main>
   );
 }
