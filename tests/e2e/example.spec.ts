a5656c194509ad97f6da6d34adb2bdfffcd1b15e
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2024-10-07 13:07:19 +0200

    Fix e2e example test

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from "@playwright/test";
 test.describe("basic example test", () => {
   test("has title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Remix Application Template");
+    await expect(page).toHaveTitle("Verfahrensplattform");
   });
 
   test("shows hello message", async ({ page }) => {
     await page.goto("/");
-    await expect(page.locator("text=DigitalService")).toBeVisible();
+    await expect(page.locator("text=Hello Verfahrensplattform!")).toBeVisible();
   });
 });
