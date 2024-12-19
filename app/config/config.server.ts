1fb7ecac1c105a5eab81ab5c42d434240475336e
Pierre Marais <pierre.marais@digitalservice.bund.de>
2024-12-19 11:44:46 +0100

    Read secret from file instead of env variable

--- app/config/config.server.ts
+++ app/config/config.server.ts
@@ -1,3 +1,5 @@
+import { readFileSync, existsSync } from 'fs';
+
 interface Config {
   BASE_URL: string;
   BRAK_IDP_OIDC_CLIENT_ID: string;
@@ -8,6 +10,9 @@ interface Config {
 
 let instance: Config | undefined = undefined;
 
+const oidcClientSecretFilePath = "/etc/brak-idp-secrets/BRAK_IDP_OIDC_CLIENT_SECRET"
+const oidcClientSecretFileExists = existsSync(oidcClientSecretFilePath)
+
 export function config(): Config {
   if (instance === undefined) {
     instance = {
@@ -15,7 +20,7 @@ export function config(): Config {
       BRAK_IDP_OIDC_CLIENT_ID:
         process.env.BRAK_IDP_OIDC_CLIENT_ID?.trim() ?? "",
       BRAK_IDP_OIDC_CLIENT_SECRET:
-        process.env.BRAK_IDP_OIDC_CLIENT_SECRET?.trim() ?? "",
+        oidcClientSecretFileExists ? readFileSync(oidcClientSecretFilePath, 'utf-8') : "",
       BRAK_IDP_OIDC_ISSUER: process.env.BRAK_IDP_OIDC_ISSUER?.trim() ?? "",
       BRAK_IDP_OIDC_REDIRECT_URI:
         process.env.BRAK_IDP_OIDC_REDIRECT_URI?.trim() ?? "",
