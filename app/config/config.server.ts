e09ba7e14b4cef1e2ddcb2ff7cd17d1f0b907a6a
Pierre Marais <pierre.marais@digitalservice.bund.de>
2024-12-19 12:25:55 +0100

    Executed formatter, linter & styler

--- app/config/config.server.ts
+++ app/config/config.server.ts
@@ -1,4 +1,4 @@
-import { readFileSync, existsSync } from 'fs';
+import { readFileSync, existsSync } from "fs";
 
 interface Config {
   BASE_URL: string;
@@ -10,8 +10,9 @@ interface Config {
 
 let instance: Config | undefined = undefined;
 
-const oidcClientSecretFilePath = "/etc/brak-idp-secrets/BRAK_IDP_OIDC_CLIENT_SECRET"
-const oidcClientSecretFileExists = existsSync(oidcClientSecretFilePath)
+const oidcClientSecretFilePath =
+  "/etc/brak-idp-secrets/BRAK_IDP_OIDC_CLIENT_SECRET";
+const oidcClientSecretFileExists = existsSync(oidcClientSecretFilePath);
 
 export function config(): Config {
   if (instance === undefined) {
@@ -19,8 +20,9 @@ export function config(): Config {
       BASE_URL: process.env.BASE_URL?.trim() ?? "",
       BRAK_IDP_OIDC_CLIENT_ID:
         process.env.BRAK_IDP_OIDC_CLIENT_ID?.trim() ?? "",
-      BRAK_IDP_OIDC_CLIENT_SECRET:
-        oidcClientSecretFileExists ? readFileSync(oidcClientSecretFilePath, 'utf-8') : "",
+      BRAK_IDP_OIDC_CLIENT_SECRET: oidcClientSecretFileExists
+        ? readFileSync(oidcClientSecretFilePath, "utf-8")
+        : "",
       BRAK_IDP_OIDC_ISSUER: process.env.BRAK_IDP_OIDC_ISSUER?.trim() ?? "",
       BRAK_IDP_OIDC_REDIRECT_URI:
         process.env.BRAK_IDP_OIDC_REDIRECT_URI?.trim() ?? "",
