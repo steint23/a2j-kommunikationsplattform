8e9d46f57a93b6c4d4a7ef1991d86a23699373a4
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-04 13:56:31 +0100

    Parsed files out of formData

--- app/routes/api/verfahren.tsx
+++ app/routes/api/verfahren.tsx
@@ -1,9 +1,27 @@
+import {
+  unstable_composeUploadHandlers,
+  unstable_createMemoryUploadHandler,
+  unstable_parseMultipartFormData,
+  unstable_createFileUploadHandler,
+} from "@remix-run/node";
 import type { ActionFunctionArgs } from "@remix-run/node";
 
 export async function action({ request }: ActionFunctionArgs) {
-  // Get the Klageeinreichung PDF & XML from the form body
+  const uploadHandler = unstable_composeUploadHandlers(
+    unstable_createFileUploadHandler({
+      maxPartSize: 5_000_000,
+      file: ({ filename }) => filename,
+    }),
+    unstable_createMemoryUploadHandler(),
+  );
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler,
+  );
+
+  const files = formData.getAll("files");
   // Validate the XML against the corresponding XJustix xsd schema
-  // Call the /verfahren endpoint in the Justiz-Backend-API to create a new Verfahren
+  // Call the /verfahren endpoint in the Justiz-Backend-API, reusing the same formData from the req to create a new Verfahren
   // Return the created verfahren_id
   return { verfahren_id: "verfahren" };
 }
