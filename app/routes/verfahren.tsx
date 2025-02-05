916049d0021e0d7b924fdeaec28f1b5d978359e1
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-05 13:47:10 +0100

    Added /verfahren route

---
+++ app/routes/verfahren.tsx
@@ -0,0 +1,15 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { getFilesFromMultipartFormData } from "~/services/fileupload.server";
+import { requireUserSession } from "~/services/session.server";
+
+export async function action({ request }: ActionFunctionArgs) {
+  await requireUserSession(request);
+
+  const files = getFilesFromMultipartFormData(request);
+  // Validate the XML against the corresponding XJustix xsd schema
+  // Call the /verfahren endpoint in the Justiz-Backend-API, reusing the same formData from the req to create a new Verfahren
+  // Return the created verfahren_id
+  return {
+    verfahren_id: "verfahren",
+  };
+}
