425da303ac3c3f24bf5e9447050a7933e79077bb
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-05 14:50:18 +0100

    Added submit button

--- app/routes/verfahren.tsx
+++ app/routes/verfahren.tsx
@@ -1,38 +1,42 @@
-import type { ActionFunctionArgs } from "@remix-run/node";
+import { useState } from "react";
+import type { ActionFunctionArgs, LoaderFunction } from "@remix-run/node";
 import { getFilesFromMultipartFormData } from "~/services/fileupload.server";
 import { requireUserSession } from "~/services/session.server";
 
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserSession(request);
+  return null;
+};
 export async function action({ request }: ActionFunctionArgs) {
   await requireUserSession(request);
 
   const files = getFilesFromMultipartFormData(request);
-  // Validate the XML against the corresponding XJustix xsd schema
-  // Call the /verfahren endpoint in the Justiz-Backend-API, reusing the same formData from the req to create a new Verfahren
-  // Return the created verfahren_id
-  return {
-    verfahren_id: "verfahren",
-  };
+  return null;
 }
 
 export default function Verfahren() {
+  const [filesSelected, setFilesSelected] = useState(false);
+
   return (
     <main className={"m-40 flex flex-col items-center"}>
       <h1 className={"ds-heading-01-bold mb-40 break-all"}>Verfahren</h1>
       <form method="post" encType="multipart/form-data">
-        <label htmlFor="files" className="">
-          Dateien hochladen
-        </label>
-        <input
-          type="file"
-          name="files"
-          id="files"
-          multiple
-          className="hidden"
-        />
-
-        <button type="submit" className={"ds-button"}>
-          Klage einreichen
-        </button>
+        <div className="flex flex-col gap-4">
+          <input
+            type="file"
+            name="files"
+            id="files"
+            multiple
+            onChange={(e) =>
+              setFilesSelected((e?.target?.files?.length || 0) > 0)
+            }
+          />
+          {filesSelected && (
+            <button type="submit" className={"ds-button"}>
+              Klage einreichen
+            </button>
+          )}
+        </div>
       </form>
     </main>
   );
