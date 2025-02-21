dcc69425b7e2ec989f759a004b895130cf3fc890
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-21 09:45:26 +0100

    Extract CreateVerfahren component

--- app/routes/verfahren.tsx
+++ app/routes/verfahren.tsx
@@ -23,44 +23,49 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Verfahren() {
-  const [filesSelected, setFilesSelected] = useState(false);
-
   return (
     <main className={"m-40 flex flex-col items-center"}>
       <h1 className={"ds-heading-01-bold mb-40 break-all"}>Verfahren</h1>
-      <form method="post" encType="multipart/form-data">
-        <div className="flex flex-col gap-4">
-          <label className="font-bold" htmlFor="xjustiz">
-            XJustiz-Datei
-          </label>
-          <input
-            className="border-2 border-black-300 p-10"
-            type="file"
-            accept=".xml"
-            name="xjustiz"
-            id="xjustiz"
-            onChange={(e) =>
-              setFilesSelected((e?.target?.files?.length || 0) > 0)
-            }
-          />
-          <label className="font-bold" htmlFor="files">
-            Anhänge
-          </label>
-
-          <input
-            className="border-2 border-black-300 p-10"
-            type="file"
-            name="files"
-            id="files"
-            multiple
-          />
-          {filesSelected && (
-            <button type="submit" className={"ds-button"}>
-              Klage einreichen
-            </button>
-          )}
-        </div>
-      </form>
+      <CreateVerfahren />
     </main>
+  );
+}
+
+function CreateVerfahren() {
+  const [filesSelected, setFilesSelected] = useState(false);
+  return (
+    <form method="post" encType="multipart/form-data">
+      <div className="flex flex-col gap-4">
+        <label className="font-bold" htmlFor="xjustiz">
+          XJustiz-Datei
+        </label>
+        <input
+          className="border-2 border-black-300 p-10"
+          type="file"
+          accept=".xml"
+          name="xjustiz"
+          id="xjustiz"
+          onChange={(e) =>
+            setFilesSelected((e?.target?.files?.length || 0) > 0)
+          }
+        />
+        <label className="font-bold" htmlFor="files">
+          Anhänge
+        </label>
+
+        <input
+          className="border-2 border-black-300 p-10"
+          type="file"
+          name="files"
+          id="files"
+          multiple
+        />
+        {filesSelected && (
+          <button type="submit" className={"ds-button"}>
+            Klage einreichen
+          </button>
+        )}
+      </div>
+    </form>
   );
 }
