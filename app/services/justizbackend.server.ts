8f2e262a11c1acd14a6b622bda76e4ed1e70a813
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-21 09:35:43 +0100

    Implemented getAllVerfahren endpoint

--- app/services/justizbackend.server.ts
+++ app/services/justizbackend.server.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 interface JustizBackendService {
   createVerfahren(xjustiz: File, files: File[]): Promise<Verfahren>;
+  getAllVerfahren(limit: number, offset: number): Promise<Verfahren[]>;
 }
 
 class JustizBackendServiceImpl implements JustizBackendService {
@@ -9,6 +10,46 @@ class JustizBackendServiceImpl implements JustizBackendService {
 
   constructor(url: string = "https://kompla.sinc.de") {
     this.baseUrl = url;
+  }
+  async getAllVerfahren(limit: number, offset: number): Promise<Verfahren[]> {
+    // Hack until SINC has a valid certificate
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    const url = `${this.baseUrl}/api/v1/verfahren?limit=${limit}&offset=${offset}`;
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        response.body
+          ?.getReader()
+          .read()
+          .then((r) =>
+            console.error("Failed to fetch Verfahren: ", r.value?.toString()),
+          );
+        throw new Error(`Failed to fetch Verfahren`);
+      }
+
+      const body = await response.json();
+      // TODO: Once the API is fixed, we can use the commented line below instead of working directly with an array.
+      // const parsedVerfahren: Verfahren[] = z.object( { data: z.array(VerfahrenSchema) }).parse(body)?.data;
+      const parsedVerfahren: Verfahren[] = z.array(VerfahrenSchema).parse(body);
+
+      console.log("Fetched Verfahren successfully:", parsedVerfahren);
+      return parsedVerfahren;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Handle zod validation errors
+        console.error("Zod validation error:", error.errors);
+      } else {
+        console.error("Error fetching Verfahren:", error);
+      }
+      throw error;
+    }
   }
 
   async createVerfahren(xjustiz: File, files: File[]): Promise<Verfahren> {
