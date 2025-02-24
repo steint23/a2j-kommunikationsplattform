fb1dc6b0dd7ba16fbabb6b38bb0ff8758bdcb5fc
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-24 11:00:29 +0100

    Add ability to show the Aktenbaum

--- app/routes/verfahren.$id.tsx
+++ app/routes/verfahren.$id.tsx
@@ -2,10 +2,23 @@ import { justizBackendService } from "~/services/servicescontext.server";
 import { useLoaderData } from "@remix-run/react";
 
 export async function loader({ params }: { params: { id: string } }) {
+  const akte = await justizBackendService.getAkte(params.id);
+
   // TODO: Get the aktenbaum here and render it
-  return params.id;
+  return akte;
 }
 export default function VerfahrenInfo() {
   const akte = useLoaderData<typeof loader>();
-  return <>Test {akte}</>;
+  return (
+    <div>
+      <div className="text-xl mb-10 font-bold">Datenraum</div>
+      {/* <div className="flex flex-col gap-20">
+        {akte.aktenteile && akte.aktenteile.map((aktenteil) => (
+            <div key={aktenteil.id} className="text-md">
+                {aktenteil.name}
+                </div>
+            ))}
+    </div> */}
+    </div>
+  );
 }
