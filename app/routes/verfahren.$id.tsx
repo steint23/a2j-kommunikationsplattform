f344ce20d13509e0824056216072f4931249bd8c
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-02-28 14:49:10 +0100

    Display documents closer to each other

--- app/routes/verfahren.$id.tsx
+++ app/routes/verfahren.$id.tsx
@@ -49,7 +49,7 @@ function AkteWithDokumente() {
           return (
             <div key={aktenteil.aktenteilId}>
               <div className="mb-10 font-medium">{aktenteil.aktenteilName}</div>
-              <div className="flex gap-x-80 gap-y-24 flex-wrap">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-y-16 flex-wrap">
                 {aktenteil.dokumente &&
                   aktenteil.dokumente.map((dokument) => {
                     return (
