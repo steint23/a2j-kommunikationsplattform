d453a8da0e32e5fbef0b3186376ed3246febea3d
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-05-19 13:14:25 +0200

    Add Prettier plugins to improve code quality
    - https://www.npmjs.com/package/prettier-plugin-organize-imports and
    - https://www.npmjs.com/package/prettier-plugin-tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  endOfLine: "auto",
+};
+
+export default config;
