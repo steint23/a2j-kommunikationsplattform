4228687fe9dc5c043af2b1a88c894e86b9595664
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-06-05 17:27:52 +0200

    Update e2e example test

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("basic example test", () => {
   test("has title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Kommunikationsplattform");
+    await expect(page).toHaveTitle("Kommunikationsplattform | Justiz-Services");
   });
 
   test("shows Kommunikationsplattform text", async ({ page }) => {
